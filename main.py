38377489b0cff3cead76fe3f683fa20a418c3c2b
Ishimwe-William <ishimwe.william2000@gmail.com>
2023-12-05 16:09:41 +0200

    tracking trucks and motorcycles also is configured

--- main.py
+++ main.py
@@ -1,75 +1,82 @@
 import cv2
 import pandas as pd
 from ultralytics import YOLO
-from tracker import *
+from tracker import Tracker
 
+# Load YOLO model
 model = YOLO('yolov8s.pt')
 
 
+# Mouse callback function
 def RGB(event, x, y, flags, param):
     if event == cv2.EVENT_MOUSEMOVE:
         colorsBGR = [x, y]
         print(colorsBGR)
 
 
+# Set up mouse callback
 cv2.namedWindow('RNP')
 cv2.setMouseCallback('RNP', RGB)
 
+# Open video capture
 cap = cv2.VideoCapture('vehicle_count.mp4')
 
-my_file = open("coco.txt", "r")
-data = my_file.read()
+# Read class names from a file
+with open("coco.txt", "r") as my_file:
+    data = my_file.read()
 class_list = data.split("\n")
-# print(class_list)
 
-count = 0
-
+# Initialize tracker
 tracker = Tracker()
 
+# Define dictionaries and counters for vehicle tracking
 vh_down = {}
 counter_down = []
-
 vh_up = {}
 counter_up = []
 
-# line y coordinates
+# Line y coordinates
 cy1 = 170
 cy2 = 190
 offset = 6
+
+# Initialize count variable
+count = 0
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
+
+    # Skip frames for faster processing
     count += 1
     if count % 3 != 0:
         continue
+
+    # Resize the frame
     frame = cv2.resize(frame, (1020, 500))
 
+    # Predict with YOLO model
     results = model.predict(frame)
-    #   print(results)
     a = results[0].boxes.data
     px = pd.DataFrame(a).astype("float")
-    #    print(px)
-    list = []
 
+    # Extract relevant information about vehicles
+    vehicles = []
     for index, row in px.iterrows():
-        #        print(row)
-
-        x1 = int(row[0])
-        y1 = int(row[1])
-        x2 = int(row[2])
-        y2 = int(row[3])
-        d = int(row[5])
+        x1, y1, x2, y2, _, d = map(int, row)
         c = class_list[d]
-        # list of vehicles needed to be detected
-        if 'car' or 'truck' or 'motorcycle' in c:
-            list.append([x1, y1, x2, y2])
-    bbox_id = tracker.update(list)
+        if 'car' in c or 'truck' in c or 'motorcycle' in c:
+            vehicles.append([x1, y1, x2, y2])
+
+    # Update tracker and process tracked vehicles
+    bbox_id = tracker.update(vehicles)
     for bbox in bbox_id:
         x3, y3, x4, y4, id = bbox
-        cx = int(x3 + x4) // 2
-        cy = int(y3 + y4) // 2
+        cx = (x3 + x4) // 2
+        cy = (y3 + y4) // 2
+
+        # Check if the vehicle crosses the lines
         if (cy + offset) > cy1 > (cy - offset):
             vh_down[id] = cy
         if id in vh_down:
@@ -90,21 +97,24 @@ while True:
                 if counter_up.count(id) == 0:
                     counter_up.append(id)
 
-    # Drawing lines
+    # Draw lines
     cv2.line(frame, (110, cy1), (1020, cy1), (255, 255, 255), 1)
-    # cv2.putText(frame, '1Line', (224, cy1 - 3), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
-
     cv2.line(frame, (70, cy2), (1020, cy2), (255, 255, 255), 1)
-    # cv2.putText(frame, '2Line', (116, cy2 - 3), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
 
+    # Display counts
     down = len(counter_down)
     cv2.putText(frame, ('Going down: ') + str(down), (60, 100), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
 
     up = len(counter_up)
-    cv2.putText(frame, ('Going up: ') + str(up), (60, 130), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
+    cv2.putText(frame, ('Going up: ') + str(up), (794, 77), cv2.FONT_HERSHEY_COMPLEX, 0.8, (0, 255, 255), 2)
 
+    # Show the frame
     cv2.imshow("RNP", frame)
+
+    # Break the loop if 'Esc' key is pressed
     if cv2.waitKey(1) & 0xFF == 27:
         break
+
+# Release resources
 cap.release()
 cv2.destroyAllWindows()
--- tracker.py
+++ tracker.py
@@ -1,6 +1,5 @@
 import math
 
-
 class Tracker:
     def __init__(self):
         # Store the center positions of the objects
@@ -21,23 +20,22 @@ class Tracker:
 
             # Find out if that object was detected already
             same_object_detected = False
-            for id, pt in self.center_points.items():
+            for obj_id, pt in self.center_points.items():
                 dist = math.hypot(cx - pt[0], cy - pt[1])
 
                 if dist < 35:
-                    self.center_points[id] = (cx, cy)
-                    #                    print(self.center_points)
-                    objects_bbs_ids.append([x, y, w, h, id])
+                    self.center_points[obj_id] = (cx, cy)
+                    objects_bbs_ids.append([x, y, w, h, obj_id])
                     same_object_detected = True
                     break
 
-            # New object is detected we assign the ID to that object
-            if same_object_detected is False:
+            # New object is detected, assign the ID to that object
+            if not same_object_detected:
                 self.center_points[self.id_count] = (cx, cy)
                 objects_bbs_ids.append([x, y, w, h, self.id_count])
                 self.id_count += 1
 
-        # Clean the dictionary by center points to remove IDS not used anymore
+        # Clean the dictionary by center points to remove IDs not used anymore
         new_center_points = {}
         for obj_bb_id in objects_bbs_ids:
             _, _, _, _, object_id = obj_bb_id
